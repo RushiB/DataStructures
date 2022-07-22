3a6230e6d7786f1f712bf47d7fb76f7ece2fa4c2
rushi <rushi@LAPTOP-9MGTP6KU>
2022-07-21 23:50:19 -0700

    Add project files.

---
+++ LinearDataStrucs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class LinearDataStrucs
+    {
+        /*
+         * https://www.geeksforgeeks.org/overview-of-data-structures-set-1-linear-data-structures/
+            Array
+            Linked List 
+            Stack
+            Queue
+         */
+
+        //TODO
+        /*  Linked List 
+            Stack
+            Queue
+         */
+
+        /// <summary>
+        /// linked list has a Head
+        /// 
+        /// each node has value and pointer to next node
+        /// note - next node pointer would be null for last node (for non circular linked list)
+        /// </summary>
+        public class ALinkedList
+        {
+             public class node
+            {
+                int value;
+                node nextNode = null;
+
+                // constructor
+                public node()
+                {
+                    value = 0;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DataStructures;
+
+MyMain();
+
+static void MyMain()
+{
+    Console.WriteLine("Entered main --> \n");
+    
+    TestAvgFunc();
+    
+    Console.WriteLine("\nDone executing main -->");
+    Console.ReadKey();
+}
+
+static void TestAvgFunc()
+{
+    List<int> a = new List<int> { 1, 2, 3 };
+    var b = new List<int> { 4, 5, 6 };
+
+    var x = new LinearDataStrucs();
+
+    SampleAvgOfLists(a, b);
+}
+
+static void SampleAvgOfLists(List<int> a, List<int> b)
+{
+    float avgA = 0;
+    float avgB = 0;
+
+    foreach (int currentItem in a)
+    {
+        avgA = avgA + currentItem;
+    }
+    avgA = avgA / a.Count;
+    foreach (int currentItem in b)
+    {
+        avgB = avgB + currentItem;
+    }
+    avgB = avgB / b.Count;
+
+    Console.WriteLine($"runningSumA = {avgA}");
+    Console.WriteLine("runningSumB = " + avgB);
+}
+
