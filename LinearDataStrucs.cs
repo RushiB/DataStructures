75183a00e72ad2b818abe223130708a917ffa26e
Rushi <rushidevbuddha@live.com>
2022-07-27 23:04:25 -0700

    Linked list initial commit

--- LinearDataStrucs.cs
+++ LinearDataStrucs.cs
@@ -28,19 +28,62 @@ namespace DataStructures
         /// each node has value and pointer to next node
         /// note - next node pointer would be null for last node (for non circular linked list)
         /// </summary>
+
+
+        public class node
+        {
+            int value;
+            node nextNode = null;
+
+            // constructor
+            public node(int val)
+            {
+                value = val;
+            }
+
+            public node GetNextNode()
+            {
+                return nextNode;
+            }
+            
+            public void SetNextNode(node next)
+            {
+                nextNode = next;
+            }
+        }
+
         public class ALinkedList
         {
-             public class node
-            {
-                int value;
-                node nextNode = null;
+            node head;
 
-                // constructor
-                public node()
+            // TODO - creation and insertion
+
+            public ALinkedList()
+            {
+                head = null;
+            }
+
+            //TODO - test if it works!!
+            public void AddNode(int x)
+            {
+                var newNode = new node(x);
+
+                if (head == null)
                 {
-                    value = 0;
+                    head = newNode;
+                }
+                else
+                {
+                    var temp = head.GetNextNode();
+                    while(temp != null)
+                    {
+                        temp = temp.GetNextNode();
+                    }
+
+                    temp.SetNextNode(newNode);
                 }
             }
+
         }
 
 
